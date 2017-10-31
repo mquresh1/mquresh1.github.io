51576767fb8dcb33a865252cb5f181b6c83dc05a
Mohammad <mquresh1@uci.edu>
2017-10-30 21:27:06 -0700

    Added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ $(document).ready(function() {
 			$('.navbar').removeClass('on-main');
 		}
 	});
+	
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event){
+		if(this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+			
+			// Store hash
+			var hash = this.hash;
+			
+			// 800 milliseconds to scroll
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				
+				// Add hash to URL when done scrolling (default behavior)
+				window.location.hash = hash;
+			});
+		}
+	});
 });
